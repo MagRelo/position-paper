ae5fa23e39cf20c787f0223eb49a2d52028765ea
Matt Lovan <magrelo404@gmail.com>
2019-06-26 11:15:35 -0400

    add seed data

--- app/server/utils/seedDb.js
+++ app/server/utils/seedDb.js
@@ -1,137 +1,95 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+const faker = require('faker');
 
 const UserModel = require('../models').UserModel;
 const QueryModel = require('../models').QueryModel;
-const LinkModel = require('../models').LinkModel;
+// const LinkModel = require('../models').LinkModel;
 
-// USERS
-const mattId = new mongoose.Types.ObjectId();
-const jimId = new mongoose.Types.ObjectId();
-const rickId = new mongoose.Types.ObjectId();
-const aliceId = new mongoose.Types.ObjectId();
-const bobId = new mongoose.Types.ObjectId();
-UserModel.create(
-  [
-    {
-      _id: mattId,
-      email: 'matt@gmail.com',
-      password: '1234',
-      name: 'Matt Lovan',
-      balance: 0
-    },
-    {
-      _id: jimId,
-      email: 'jim@gmail.com',
-      password: '1234',
-      name: 'Jim Jones',
-      balance: 0
-    },
-    {
-      _id: rickId,
-      email: 'rick@gmail.com',
-      password: '1234',
-      name: 'Rick Dudley',
-      balance: 0
-    },
-    {
-      _id: aliceId,
-      email: 'alice@gmail.com',
-      password: '1234',
-      name: 'Alice',
-      balance: 0
-    },
-    {
-      _id: bobId,
-      email: 'bob@gmail.com',
-      password: '1234',
-      name: 'Bob',
-      balance: 0
-    }
-  ],
-  (err, docs) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.info('%d users were successfully stored.', docs.length);
-    }
-  }
-);
+// Load seeded users, then
+// 1. Create queries
+// 2. Create links
+// 3. Create responses
 
-// QUERY
-const frontEndId = new mongoose.Types.ObjectId();
-const marketingId = new mongoose.Types.ObjectId();
-QueryModel.create(
-  [
-    {
-      _id: frontEndId,
-      title: 'Front-end Developer',
-      bonus: '10000',
+// get users and make them a query
+async function createQueries() {
+  console.log('hit');
+
+  // remove queries
+  await QueryModel.remove({});
+
+  // get users
+  const users = await UserModel.find({}).lean();
+
+  // create query objects
+  const queryObjects = users.map((user, index) => {
+    const title =
+      faker.company.catchPhraseAdjective() +
+      ' ' +
+      faker.company.catchPhraseNoun();
+    const bonus = faker.finance.amount();
+
+    return {
+      user: user._id,
+      title: title,
+      bonus: bonus,
       type: 'general',
       data: {
-        title: 'Front-end Developer',
-        bonus: '10000',
-        description: 'React, Redux'
-      },
-      user: bobId
-    },
-    {
-      _id: marketingId,
-      title: 'Marketing Executive',
-      bonus: '80000',
-      type: 'general',
-      data: {
-        title: 'Marketing Executive',
-        bonus: '80000',
-        description: 'Lead an exicitng new project'
-      },
-      user: bobId
-    }
-  ],
-  (err, docs) => {
+        title: title,
+        description: faker.lorem.paragraph(),
+        bonus: bonus
+      }
+    };
+  });
+
+  // Insert
+  await QueryModel.create(queryObjects, (err, docs) => {
     if (err) {
       console.error(err);
     } else {
       console.info('%d queries were successfully stored.', docs.length);
     }
-  }
-);
+  });
+
+  // end
+}
+createQueries();
 
 // LINK
 // const frontEndId = new mongoose.Types.ObjectId();
 // const marketingId = new mongoose.Types.ObjectId();
 
-QueryModel.create(
-  [
-    {
-      _id: frontEndId,
-      title: 'Front-end Developer',
-      bonus: '10000',
-      type: 'general',
-      data: {
-        title: 'Front-end Developer',
-        bonus: '10000',
-        description: 'React, Redux'
-      },
-      user: bobId
-    },
-    {
-      _id: marketingId,
-      title: 'Marketing Executive',
-      bonus: '80000',
-      type: 'general',
-      data: {
-        title: 'Marketing Executive',
-        bonus: '80000',
-        description: 'Lead an exicitng new project'
-      },
-      user: bobId
-    }
-  ],
-  (err, docs) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.info('%d queries were successfully stored.', docs.length);
-    }
-  }
-);
+// QueryModel.create(
+//   [
+//     {
+//       _id: frontEndId,
+//       title: 'Front-end Developer',
+//       bonus: '10000',
+//       type: 'general',
+//       data: {
+//         title: 'Front-end Developer',
+//         bonus: '10000',
+//         description: 'React, Redux'
+//       },
+//       user: bobId
+//     },
+//     {
+//       _id: marketingId,
+//       title: 'Marketing Executive',
+//       bonus: '80000',
+//       type: 'general',
+//       data: {
+//         title: 'Marketing Executive',
+//         bonus: '80000',
+//         description: 'Lead an exicitng new project'
+//       },
+//       user: bobId
+//     }
+//   ],
+//   (err, docs) => {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//       console.info('%d queries were successfully stored.', docs.length);
+//     }
+//   }
+// );
