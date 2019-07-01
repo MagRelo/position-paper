a98e4fbd603a642d5c53864fe9de2048463e5025
Matt Lovan <magrelo404@gmail.com>
2019-07-01 12:35:30 -0400

    social grid

--- app/src/components/userSocial.js
+++ app/src/components/userSocial.js
@@ -4,19 +4,21 @@ import SocialIcon from 'components/socialButton';
 
 function SocialButton(props) {
   return (
-    <div className="">
+    <div className="social-grid">
       <div className="icon-block">
         <SocialIcon company="gmail" />
         <span className="icon-label">Not Connected</span>
       </div>
-
+      <div className="icon-block">
+        <SocialIcon company="linkedin" />
+        <span className="icon-label">Not Connected</span>
+      </div>
       <div className="icon-block">
         <SocialIcon company="twitter" />
         <span className="icon-label">Not Connected</span>
       </div>
-
       <div className="icon-block">
-        <SocialIcon company="linkedin" />
+        <SocialIcon company="instagram" />
         <span className="icon-label">Not Connected</span>
       </div>
     </div>
