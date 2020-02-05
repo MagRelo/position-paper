c32dec25d00d42b24c2a29219c7ab57fb64c8036
Matt Lovan <magrelo404@gmail.com>
2020-02-05 15:42:49 -0700

    fix server bug

--- app/server/api.js
+++ app/server/api.js
@@ -132,7 +132,7 @@ router.get('/user/jobs/:jobBoardId', async function(req, res) {
   const user = await UserModel.findOne({ jobBoardId: req.params.jobBoardId });
 
   if (!user) {
-    res.status(404).send({});
+    return res.status(404).send({});
   }
 
   const results = await LinkModel.find({
